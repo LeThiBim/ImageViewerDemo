24b7c626c07fcbad160a551e709f885dd39ec0f8
LeThiBim <0812182@gmail.com>
2014-01-03 15:39:22 +0700

    - Hide/Show toolbar, status bar & navigation bar with single tap

--- ImageViewerDemo/ViewControllers/ImageViewController.m
+++ ImageViewerDemo/ViewControllers/ImageViewController.m
@@ -21,6 +21,12 @@
 @property (strong, nonatomic) UIImageView* imageView3;
 
 @property (assign, nonatomic) BOOL  isDisableScrollView;
+
+@property (nonatomic) BOOL statusBarHidden;
+@property (nonatomic, strong) UITapGestureRecognizer *tapGestureRecognizer;
+@property (nonatomic, strong) UITapGestureRecognizer *doubleTapGestureRecognizer;
+@property (nonatomic, strong) UIPinchGestureRecognizer *pinchGestureRecognizer;
+
 @end
 
 @implementation ImageViewController
@@ -56,14 +62,33 @@
 
 - (void) viewDidAppear:(BOOL)animated
 {
+    [super viewDidAppear:animated];
+    
     self.navigationController.navigationBar.translucent = YES;
     self.navigationController.toolbar.translucent = YES;
 }
 
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    if (self.tapGestureRecognizer == nil)
+    {
+        self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTapGesture:)];
+        [self.scrollView addGestureRecognizer:self.tapGestureRecognizer];
+    }
+}
+
 - (void) viewWillDisappear:(BOOL)animated
 {
+    [super viewWillDisappear:animated];
+    
     self.navigationController.navigationBar.translucent = NO;
     self.navigationController.toolbar.translucent = NO;
+    
+    if (self.toolBar.hidden) {
+        [self _updateBars:0.0];
+    }
 }
 
 #pragma mark - SOME FUNCTIONS TO HANDLE ROTATE EVENT
@@ -159,6 +184,54 @@
     }
 
 }
+
+#pragma mark - SOME FUNCTIONS FOR HANDLE SINGLE TAP
+
+- (void) handleSingleTapGesture:(UIGestureRecognizer *)gestureRecognizer {
+    [self _updateBars:0.25];
+}
+
+- (void) _updateBars:(NSTimeInterval)animationDuration {
+    BOOL hidden = !self.toolBar.hidden; // Flip our hidden state
+    
+    if (hidden && self.navigationController.topViewController != self) {
+        NSLog(@"%s: Asked to hide bar, but not the top view controller, skipping update of bars", __PRETTY_FUNCTION__);
+        return;
+    }
+    
+    // Animate the alpha for navbar and toolbar, and animate status bar's hidden state
+    void (^animationBlock)() = ^{
+        CGFloat alpha = hidden ? 0.0 : 1.0;
+        [[UIApplication sharedApplication] setStatusBarHidden:hidden];
+        self.statusBarHidden = hidden;
+        if ([self respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)]) {
+            [self setNeedsStatusBarAppearanceUpdate];
+        }
+        if (hidden == NO) {
+            self.toolBar.hidden = NO;
+            [self.navigationController setNavigationBarHidden:NO animated:NO];
+        }
+        self.toolBar.alpha =  alpha;
+        self.navigationController.navigationBar.alpha = alpha;
+    };
+    
+    // If we're hiding, after animation completes really make navbar and statusbar hidden
+    void (^completionBlock)(BOOL finished) = ^(BOOL finished) {
+        if (finished && hidden) {
+            self.toolBar.hidden = YES;
+            [self.navigationController setNavigationBarHidden:YES animated:NO];
+        }
+    };
+    
+    if (animationDuration > 0.0) {
+        [UIView animateWithDuration:animationDuration animations:animationBlock completion:completionBlock];
+    }
+    else {
+        animationBlock();
+        completionBlock(YES);
+    }
+}
+
 
 
 @end
